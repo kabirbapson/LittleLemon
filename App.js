e3885fc260be6aa84917efc5c7324d1f509f8256
Bappi Kabir <kabirbapson@gmail.com>
2023-04-27 22:47:05 +0100

    fix: lounge screen bg fix

--- App.js
+++ App.js
@@ -47,6 +47,7 @@ export default function App() {
     } catch (error) {
       console.log(error);
     }
+    
 
     // Fetch the menu from the API_URL endpoint. You can visit the API_URL in your browser to inspect the data returned
     // The category field comes as an object with a property called "title". You just need to get the title value and set it under the key "category".
