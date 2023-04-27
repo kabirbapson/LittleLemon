8406467021bd71f82758e2f806a29e5a706f0c73
Bappi Kabir <kabirbapson@gmail.com>
2023-04-27 22:48:01 +0100

    fix: lounge screen bg fix

--- App.js
+++ App.js
@@ -50,6 +50,7 @@ export default function App() {
 
 
     
+
     // Fetch the menu from the API_URL endpoint. You can visit the API_URL in your browser to inspect the data returned
     // The category field comes as an object with a property called "title". You just need to get the title value and set it under the key "category".
     // So the server response should be slighly transformed in this function (hint: map function) to flatten out each menu item in the array,
