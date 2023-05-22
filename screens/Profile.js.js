fc4bcdfa644e259d48394e9193e37385437d447c
Bappi Kabir <kabirbapson@gmail.com>
2023-05-23 00:04:26 +0100

    add/remove contact

--- screens/Profile.js.js
+++ screens/Profile.js.js
@@ -45,6 +45,7 @@ export default function Profile() {
   };
   console.log("msjsjjs", user.number);
   
+  
   return (
     <SafeAreaView
       style={{ backgroundColor: "#fff", marginHorizontal: 4, flex: 1 }}
