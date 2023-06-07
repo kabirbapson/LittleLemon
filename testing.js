2c094201a4211c55c35cc77fd57aa6e918df6126
Bappi Kabir <kabirbapson@gmail.com>
2023-06-07 03:45:40 +0100

    final commit

---
+++ testing.js
@@ -0,0 +1,8 @@
+const fruits = ["cheese", "pie", "lunch", "breakfast"];
+const string = "i am really hungry for some";
+console.log(string)
+var really = string.search('really')
+console.log(really)
+word = string.substr(really,6)
+newString = string.replace(word, word.toUpperCase())
+console.log(newString)
