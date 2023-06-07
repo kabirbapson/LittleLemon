464cb6db6bef226a3240ae96d1c721c8aafd029f
Bappi Kabir <kabirbapson@gmail.com>
2023-06-07 22:52:46 +0100

    final commit

--- testing.js
+++ testing.js
@@ -3,6 +3,7 @@ const string = "i am really hungry for some";
 console.log(string)
 var really = string.search('really')
 console.log(really)
+
 word = string.substr(really,6)
 newString = string.replace(word, word.toUpperCase())
 console.log(newString)
