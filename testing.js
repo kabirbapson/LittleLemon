7292266363aa66fa607181c673e6874753dcdebb
Bappi Kabir <kabirbapson@gmail.com>
2023-06-20 14:39:11 +0100

    ref: iOS linking & DVO Lounge

--- testing.js
+++ testing.js
@@ -2,23 +2,11 @@ const fruits = ["cheese", "pie", "lunch", "breakfast"];
 const string = "i am really hungry for some";
 
 function addNum(min, max) {
-
-  
   var range = max - min + 1;
 
-
-
-
-
-  
   var rand = Math.random();
-  
 
-
-  
   return min + Math.floor(range * rand);
 }
-
-
 
 console.log(fruits[addNum(0, 3)]);
