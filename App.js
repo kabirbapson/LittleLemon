a2c662e56fca8670aa832ee17e74f7e724134d7e
Bappi Kabir <kabirbapson@gmail.com>
2023-07-07 00:32:29 +0100

    getting onn

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ export default function App() {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
   useEffect(() => {
+    
     AsyncStorage.getItem("profile")
       .then((user) => {
         // console.log(user, "App");
