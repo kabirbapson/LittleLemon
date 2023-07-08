849c9c36dd996e77b4d785133d06ad13468d48e6
Bappi Kabir <kabirbapson@gmail.com>
2023-07-08 23:29:01 +0100

    getting onn

--- testing.js
+++ testing.js
@@ -4,9 +4,10 @@ const string = "i am really hungry for some";
 function addNum(min, max) {
   var range = max - min + 1;
 
-  
+
   var rand = Math.random();
 
+  
   return min + Math.floor(range * rand);
 }
 
