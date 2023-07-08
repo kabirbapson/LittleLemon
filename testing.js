7fbc54806e7e46909f5c13830e5ef15b42eec264
Bappi Kabir <kabirbapson@gmail.com>
2023-07-08 21:48:31 +0100

    getting onn

--- testing.js
+++ testing.js
@@ -1,11 +1,9 @@
 const fruits = ["cheese", "pie", "lunch", "breakfast"];
 const string = "i am really hungry for some";
 
-
 function addNum(min, max) {
   var range = max - min + 1;
 
-  
   var rand = Math.random();
 
   return min + Math.floor(range * rand);
