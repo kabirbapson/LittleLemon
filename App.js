d3338778f2f363461eafe6c0987324e88525532d
Bappi Kabir <kabirbapson@gmail.com>
2023-09-04 23:17:37 +0100

    exc3

--- App.js
+++ App.js
@@ -9,7 +9,6 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import Home from "./screens/Home";
 const Stack = createNativeStackNavigator();
 
-
 export default function App() {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
